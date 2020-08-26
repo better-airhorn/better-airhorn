91d4d724f93ff5430a45def9c209dd8251c44b84
Clemens Ertle <clemensertle@gmail.com>
2020-08-26 18:12:51 +0200

    feat(bot): add soundinfo command

--- apps/bot/src/utils/Utils.ts
+++ apps/bot/src/utils/Utils.ts
@@ -23,3 +23,8 @@ export function filterInt(value: string): number {
 	}
 	return NaN;
 }
+
+export function humanFileSize(size: number): string {
+	const i = size === 0 ? 0 : Math.floor(Math.log(size) / Math.log(1024));
+	return `${Number((size / Math.pow(1024, i)).toFixed(2))} ${['B', 'kB', 'MB', 'GB'][i]}`;
+}
