907a3ec942c80251fc36ef3cb7f7465ba3656565
Clemens Ertle <clemensertle@gmail.com>
2020-09-20 16:05:34 +0200

    feat(bot): add LockDisplay to show held locks

--- apps/bot/src/commands/commands.ts
+++ apps/bot/src/commands/commands.ts
@@ -14,6 +14,7 @@ import { RandomCommand } from './music/Random';
 import { SoundInfoCommand } from './music/SoundInfo';
 import { ECommand } from './owner/E';
 import { ExecCommand } from './owner/Exec';
+import { LockDisplayCommand } from './owner/LockDisplay';
 import { TimeCommand } from './owner/Time';
 
 [
@@ -35,4 +36,5 @@ import { TimeCommand } from './owner/Time';
 	ImportCommand,
 	RandomCommand,
 	SoundInfoCommand,
+	LockDisplayCommand,
 ];
