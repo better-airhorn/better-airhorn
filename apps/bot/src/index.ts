c47a2c6f635d5d78712344ab0e6ab7acbcdc181a
Clemens Ertle <clemensertle@gmail.com>
2021-04-12 18:44:50 +0200

    revert(bot): move back .env checking

--- apps/bot/src/index.ts
+++ apps/bot/src/index.ts
@@ -1,6 +1,8 @@
 import { Guild as GuildEntity, GuildSetting, Like, SoundCommand, Statistic, Usage } from '@better-airhorn/entities';
 import { Util } from 'discord.js';
 import 'dotenv/config';
+import { readFileSync } from 'fs';
+import { join } from 'path';
 import 'reflect-metadata';
 import { createConnection } from 'typeorm';
 import { BAClient } from './client/BAClient';
@@ -10,10 +12,17 @@ import './services/events/LoggingEvents';
 import { services } from './services/services';
 import { isDev } from './utils/isEnvironment';
 import { logger, TypeORMLogger } from './utils/Logger';
-import { ensureDatabaseExtensions } from './utils/Utils';
+import { ensureDatabaseExtensions, parseEnvExample } from './utils/Utils';
 
 // eslint-disable-next-line @typescript-eslint/no-require-imports
 require('appmetrics-dash').monitor({ port: Config.misc.appmetricsPort });
+
+const matches = parseEnvExample(readFileSync(join(__dirname, '../env.example')).toString());
+for (const key of matches) {
+	if (!(key in process.env) || process.env[key]?.length === 0) {
+		logger.warn(`missing env variable: ${key}`);
+	}
+}
 
 (async (): Promise<void> => {
 	await createConnection({
