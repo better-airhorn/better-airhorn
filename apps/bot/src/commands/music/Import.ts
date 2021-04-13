78b64ddcd5ece317d8381a16b26833366bd96e40
Clemens Ertle <clemensertle@gmail.com>
2021-04-13 21:18:36 +0200

    feat(bot): search for replied messages in import command

--- apps/bot/src/commands/music/Import.ts
+++ apps/bot/src/commands/music/Import.ts
@@ -1,5 +1,6 @@
 import { convertToOGG } from '@better-airhorn/audio';
 import { Command, CommandBase, Message, UseGuard } from '@better-airhorn/shori';
+import { Collection, MessageAttachment, Snowflake } from 'discord.js';
 import { Config } from '../../config/Config';
 import { HealthCheckGuard } from '../../guards/HealthCheckGuard';
 import { importAudioFile } from '../../utils/prompts/SoundCommandPrompts';
@@ -8,40 +9,38 @@ import { humanFileSize, wrapInCodeBlock } from '../../utils/Utils';
 @Command('import', {
 	channel: 'any',
 	category: 'music',
-	description: 'imports a sound from the prior messages',
+	description: 'imports a sound from the prior messages or from a replied messages',
 	example: 'import https://www.youtube.com/watch?v=dQw4w9WgXcQ',
 	parseArguments: true,
 })
 export class ImportCommand extends CommandBase {
 	@UseGuard(HealthCheckGuard)
 	public async exec(message: Message): Promise<any> {
-		const messages = await message.channel.messages.fetch({ limit: 30 });
-		const messagesWithAttachments = messages
-			.sort((a, b) => b.createdTimestamp - a.createdTimestamp)
-			.filter(msg => msg.attachments.size > 0);
+		let suitable: MessageWithAttachment;
+		if (message.reference?.messageID) {
+			const refMessage = (await message.channel.messages.fetch(message.reference.messageID)) as Message;
+			if (refMessage.attachments.size < 1) {
+				return message.warn("I can't find any attachments on the message you referenced");
+			}
 
-		if (messagesWithAttachments.size < 1) {
-			return message.warn(
-				"I can't find any message with attachments",
-				'I looked trough the last 30 messages in this channel',
+			const suitableAttachment = refMessage.attachments.find(x =>
+				convertToOGG.supportedFormats.includes(x.name!.split('.').pop()!),
 			);
+			if (!suitableAttachment) {
+				return message.warn(
+					`I can't find any attachments with supported file formats.\nmake sure its format is one of those:\n${wrapInCodeBlock(
+						convertToOGG.supportedFormats.join('\n'),
+					)}`,
+				);
+			}
+			suitable = { attach: suitableAttachment, msg: refMessage };
+		} else {
+			const messages = (await message.channel.messages.fetch({ limit: 30 })) as Collection<Snowflake, Message>;
+			const msg = await this.findSuitableMessage(messages, message);
+			if (!msg) return;
+			suitable = msg;
 		}
 
-		const messagesWithSuitableFormats = messagesWithAttachments
-			.map(msg => ({
-				attach: msg.attachments.find(x => convertToOGG.supportedFormats.includes(x.name!.split('.').pop()!))!,
-				msg,
-			}))
-			.filter(entry => entry.attach);
-		if (messagesWithSuitableFormats.length < 1) {
-			return message.warn(
-				`I can't find any attachments with supported file formats.\nmake sure its format is one of those:\n${wrapInCodeBlock(
-					convertToOGG.supportedFormats.join('\n'),
-				)}`,
-			);
-		}
-
-		const suitable = messagesWithSuitableFormats[0];
 		if (suitable.attach?.size > Config.files.maxFileSize) {
 			return message.error(
 				`[The Attachment](${suitable.msg.url}) I found is too big`,
@@ -51,4 +50,42 @@ export class ImportCommand extends CommandBase {
 
 		await importAudioFile({ attachment: suitable.attach, message: suitable.msg as Message });
 	}
+
+	private async findSuitableMessage(
+		messages: Collection<Snowflake, Message>,
+		originalMessage: Message,
+	): Promise<MessageWithAttachment | null> {
+		const messagesWithAttachments = messages
+			.sort((a, b) => b.createdTimestamp - a.createdTimestamp)
+			.filter(msg => msg.attachments.size > 0);
+
+		if (messagesWithAttachments.size < 1) {
+			await originalMessage.warn(
+				"I can't find any message with attachments",
+				'I looked trough the last 30 messages in this channel',
+			);
+			return null;
+		}
+
+		const messagesWithSuitableFormats = messagesWithAttachments
+			.map(msg => ({
+				attach: msg.attachments.find(x => convertToOGG.supportedFormats.includes(x.name!.split('.').pop()!))!,
+				msg,
+			}))
+			.filter(entry => entry.attach);
+		if (messagesWithSuitableFormats.length < 1) {
+			await originalMessage.warn(
+				`I can't find any attachments with supported file formats.\nmake sure its format is one of those:\n${wrapInCodeBlock(
+					convertToOGG.supportedFormats.join('\n'),
+				)}`,
+			);
+			return null;
+		}
+		return messagesWithSuitableFormats[0];
+	}
+}
+
+interface MessageWithAttachment {
+	attach: MessageAttachment;
+	msg: Message;
 }
