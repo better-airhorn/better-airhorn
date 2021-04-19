b9d9700de7586e23458749ab43995627ab2ff873
Clemens Ertle <clemensertle@gmail.com>
2021-04-19 18:34:30 +0200

    refactor(bot): switching logger

--- apps/bot/src/utils/Logger.ts
+++ apps/bot/src/utils/Logger.ts
@@ -1,51 +1,15 @@
-/* eslint-disable @typescript-eslint/no-require-imports */
+import { createLogger as createNewLogger } from 'bunyan';
 import { Logger } from 'typeorm';
-import { createLogger, format, transports } from 'winston';
-import LokiTransport from 'winston-loki';
-import { ConsoleTransportInstance } from 'winston/lib/winston/transports';
 import { Config } from '../config/Config';
-import { isProd } from './isEnvironment';
-const usedTransports: { loki?: LokiTransport; console: ConsoleTransportInstance } = {
-	console: new transports.Console({
-		format: format.combine(
-			format.colorize(),
-			format.splat(),
-			format.printf(
-				info =>
-					`[${info.level}] : ${info?.labels?.source ? `[${info.labels.source}] ` : ''}${
-						typeof info.message === 'string' ? info.message : JSON.stringify(info.message)
-					}`,
-			),
-		),
-		level: Config.logging.level,
-	}),
-};
 
-if (isProd()) {
-	usedTransports.loki = new LokiTransport({
-		host: Config.credentials.loki.url,
-		labels: { job: 'better-airhorn-bot' },
-		level: Config.logging.level,
-	});
-}
-
-const winston = createLogger({
-	// @ts-ignore
-	transports: Object.values(usedTransports),
-});
-export const logger = winston;
+export const logger = createNewLogger({ name: 'ba', level: Config.logging.level });
 
 export function getSubLogger(source: string) {
-	return logger.child({ labels: { source } });
-}
-
-export function changeLogLevel(level: 'debug' | 'info' | 'error') {
-	usedTransports.console.level = level;
-	if (usedTransports.loki) usedTransports.loki.level = level;
+	return logger.child({ module: source });
 }
 
 export class TypeORMLogger implements Logger {
-	private readonly childLogger = logger.child({ labels: { source: 'typeorm' } });
+	private readonly childLogger = getSubLogger('typeorm');
 
 	public logQuery(query: string, parameters?: any[]) {
 		this.childLogger.debug(query, parameters);
@@ -68,6 +32,19 @@ export class TypeORMLogger implements Logger {
 	}
 
 	public log(level: 'log' | 'info' | 'warn', message: any) {
-		this.childLogger.log(level, message);
+		switch (level) {
+			case 'log':
+				logger.debug(message);
+				break;
+			case 'info':
+				logger.info(message);
+				break;
+			case 'warn':
+				logger.warn(message);
+				break;
+			default:
+				logger.warn('typeorm sent unknown log level: %s', level);
+				break;
+		}
 	}
 }
